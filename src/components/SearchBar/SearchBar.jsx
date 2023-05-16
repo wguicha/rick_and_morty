c2ef450662bcf777bf5cd80fff50b636de14bebf
WilliamG <williamalberto1@hotmail.com>
2023-05-16 16:49:08 -0500

    Ok stylos search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,14 @@
+import styles from './SearchBar.module.css'
+
 export default function SearchBar(props) {
    const { onSearch } = props;
    return (
-      <div>
-         <input type='search' />
-         <button onClick={onSearch}>Agregar</button>
+      <div className={styles.divWrap}>
+         <div className={styles.divSearch}>
+            <input className={styles.inputSearch} type='search' />
+            <button className={styles.buttonSearch} onClick={onSearch}>Agregar</button>
+            <i className="fa fa-search"></i>
+         </div>
       </div>
    );
 }
