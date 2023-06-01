c31cd89faf40c12b2840ceedeff23400e30d1f24
WilliamG <williamalberto1@hotmail.com>
2023-05-31 20:11:25 -0500

    con build sin warnings

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,9 @@
 import Card from '../Card/Card';
-import styles from './Cards.module.css'
 
 export default function Cards(props) {
    const { characters, onClose } = props;
    return (
-      <div className={styles.divCards}>{
+      <div>{
          characters.map((character) => {
             return <Card id={character.id}
                key={character.id}
