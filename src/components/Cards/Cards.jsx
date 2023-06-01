c793f3c815de0b15a9ade1acfc8ca6d67ce96184
WilliamG <williamalberto1@hotmail.com>
2023-05-31 20:24:27 -0500

    prueba para poder cargar en vercel

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,10 @@
 import Card from '../Card/Card';
+import styles from './Cards.module.css'
 
 export default function Cards(props) {
    const { characters, onClose } = props;
    return (
-      <div>{
+      <div className={styles.divCards}>{
          characters.map((character) => {
             return <Card id={character.id}
                key={character.id}
